02fe23b8b80f9073cde2daa48a12b8c77bca4a96
*Kim Zick (rummik) <k@9k1.us>
2018-12-29 15:26:48 -0500

    Add Grunt task for running Mocha tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,13 +24,19 @@ module.exports = function(grunt) {
             purgebadges: {
                 command: 'node_modules/.bin/camo-purge'
             }
+        },
+        mochaTest: {
+            test: {
+                src: [ 'test/**/*.js' ]
+            }
         }
     });
 
+    grunt.loadNpmTasks('grunt-mocha-test');
     grunt.loadNpmTasks('grunt-xo');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-shell');
-    grunt.registerTask('test', ['xo', 'shell:purgebadges']);
+    grunt.registerTask('test', ['xo', 'shell:purgebadges', 'mochaTest' ]);
 
     grunt.loadNpmTasks('grunt-jsdoc-to-markdown');
     grunt.registerTask('doc', ['jsdoc2md', 'concat']);
